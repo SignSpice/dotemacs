f3b6a6cba651b3bc8f265537ccfc7cb934fd10fc
SignSpice <little.alaskan@protonmail.com>
2022-07-15 21:36:46 -0800

    progress

---
+++ early-init.el
@@ -0,0 +1,12 @@
+;; Skip garbage collections during startup to speed things up. This is optional
+;; but nice to have.
+
+(setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
+      gc-cons-percentage 0.6)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold 33554432 ; 32mb
+                  gc-cons-percentage 0.1)))
+
+(setq package-enable-at-startup nil)
